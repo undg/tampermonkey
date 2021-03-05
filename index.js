64fdce8834b07de5082cae35372780d6b4dc0217
und3rdg <und3rdg@gmail.com>
2021-03-05 17:38:45 +0000

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         typeRacerSound
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  add countdown sound for the last 3 sec.
+// @author       undg
+// @match        https://play.typeracer.com/
+// @require      file:////home/undg/Code/tampermonkey/typeRacerSound/index.js
+// ==/UserScript==
+
+;(function () {
+    'use strict'
+
+    const body = document.querySelector('body')
+
+    const audioBeep = new Audio(
+        ''
+    )
+
+    const config = {
+        childList: true,
+    }
+
+
+    new MutationObserver((mutationrecordsBody) => {
+        const timeNode = mutationrecordsBody[0]?.addedNodes[0]?.querySelector('.popupContent .time')
+        if (timeNode) observeTimePopup(timeNode, config)
+    }).observe(body, config)
+
+    function observeTimePopup(timeNode, config) {
+        const PopupObserver = new MutationObserver(playCountdownSound)
+        PopupObserver.observe(timeNode, config)
+    }
+
+    function playCountdownSound(mutationrecordsPopup) {
+        const cntDown = +mutationrecordsPopup[0]?.target?.innerText.replace(/:/, '')
+        if (cntDown === 4) audioBeep.play()
+    }
+})()
