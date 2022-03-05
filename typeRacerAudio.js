c260949aed96631dd419ebad4e562a2d48ee78ea
und3rdg <und3rdg@gmail.com>
2022-03-05 19:10:27 +0000

    typeRacerAudio: Implement volume changer, indicator and test play.
    Use mouse wheel on volume icon in header to change volume,
    click icon to play sound.

--- typeRacerAudio.js
+++ typeRacerAudio.js
@@ -13,6 +13,27 @@
     const body = document.querySelector('body')
 
     const audioBeep = new Audio('https://github.com/undg/typeRacerAudio/raw/slave/assets/typeracer.wav')
+    
+    const volume = localStorage.getItem('volume') ?? 4
+
+    const titleElement = document.getElementsByClassName('title')[0]
+    const volElement = document.createElement('span')
+
+    volElement.id = 'volElement'
+    volElement.innerText = volumeText(volume)
+    volElement.style.padding = '10px'
+    volElement.style.backgroundColor = '#444'
+    volElement.style.borderRadius = '20px'
+
+    // Events
+    volElement.onwheel = volChange
+    volElement.addEventListener('click', e => {
+        e.preventDefault()
+        setAudio(audioBeep)
+        audioBeep.play()
+    })
+
+    titleElement.appendChild(volElement)
 
     const config = {
         childList: true,
@@ -30,6 +51,56 @@
 
     function playCountdownSound(mutationrecordsPopup) {
         const cntDown = +mutationrecordsPopup[0]?.target?.innerText.replace(/:/, '')
+        setAudio(audioBeep)
         if (cntDown === 3) setTimeout(()=>audioBeep.play(), 400)
+    }
+
+    /*
+     * @param {HTMLAudioElement} audioBeep
+     */
+    function setAudio(audioBeep) {
+        const volume = localStorage.getItem('volume') ?? 4
+        localStorage.setItem('volume', volume)
+        audioBeep.volume = volume / 10
+    }
+
+    /*
+     * @param {(number|string|undefined)} volume
+     */
+
+    function volumeText(volume) {
+        volume = +volume
+        if(volume === 0)
+            return "🔇 "
+        if(volume > 0)
+            return "🔈 " + volume.toString() + "0%"
+        if(volume > 4)
+            return "🔉 " + volume.toString() + "0%"
+        if(volume > 7)
+            return "🔊 " + volume.toString() + "0%"
+        if(volume === 10)
+            return "🔊 MAX"
+        return "🔊 " + volume.toString() + "0%"
+    }
+
+    /*
+     * @param {WheelEvent} event
+     * 
+     * @return void
+     */
+    function volChange(event) {
+        event.preventDefault()
+
+        const element = document.getElementById('volElement')
+
+        let volume = +localStorage.getItem('volume')
+        if(event.deltaY < 0 && volume < 10) 
+            volume++
+        if(event.deltaY > 0 && volume > 0) 
+            volume--
+
+        element.innerText = volumeText(volume)
+        localStorage.setItem('volume', volume)
+        setAudio(audioBeep)
     }
 })()
