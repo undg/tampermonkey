9700236ed8a56abc8e589ce3c4827f739410370c
undg <und3rdg@gmail.com>
2023-03-03 16:51:25 +0000

    Revert notification in airtable-copy-branch-name

--- airtable-copy-branch-name.js
+++ airtable-copy-branch-name.js
@@ -10,14 +10,22 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    document.querySelectorAll("[data-columntype='formula']")
-        .forEach(_=>document.addEventListener('click', ({target:{innerText}})=>{
-        const txt = 'git checkout -b ' + innerText
-        navigator.clipboard.writeText(txt)
-        // notifySend(innerText)
-        console.log(txt)
-    }))
+    "use strict";
+    console.log("airtable script");
+
+    document.querySelectorAll("[data-columntype='formula']").forEach(() => {
+        console.log("airtable adding addEventListener");
+
+        document.addEventListener("click", (e) => {
+            const txt = "git checkout -b " + e.target.innerText;
+
+            navigator.clipboard.writeText(txt);
+
+            notifySend(e.target.innerText);
+
+            console.log(txt);
+        });
+    });
 })();
 
 function notifySend(msg) {
@@ -27,22 +35,17 @@ function notifySend(msg) {
     } else if (Notification.permission === "granted") {
         // Check whether notification permissions have already been granted;
         // if so, create a notification
-        const notification = new Notification(msg);
-        // …
-    }
-    /*
-    else if (Notification.permission !== "denied") {
+        // const notification = new Notification(msg);
+        new Notification(msg);
+    } else if (Notification.permission !== "denied") {
         // We need to ask the user for permission
         Notification.requestPermission().then((permission) => {
             // If the user accepts, let's create a notification
             if (permission === "granted") {
-                // const notification = new Notification("Hi there!");
-                // …
+                new Notification(msg);
             }
         });
     }
-    */
-
     // At last, if the user has denied notifications, and you
     // want to be respectful there is no need to bother them anymore.
 }
